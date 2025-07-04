f883572b929dee0fffb9ce46de6dd01a4a7c1cd5
Anderson Coelho <anderson.coelho1969@gmail.com>
2025-07-03 21:43:12 -0400

    chore: change models to implement auth

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, Permission
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permission
 
 from companies.models import Enterprise
 
