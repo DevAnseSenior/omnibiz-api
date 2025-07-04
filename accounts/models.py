70dcd6e8e48599beaf2cd42dd4770c95d38f8f83
Anderson Coelho <anderson.coelho1969@gmail.com>
2025-07-03 20:35:11 -0400

    chore: new models Group, GroupPermissions and UserGroups

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, Permission
 
+from companies.models import Enterprise
+
 class User(AbstractBaseUser):
     name = models.CharField(max_length=150)
     email = models.EmailField(unique=True)
@@ -10,3 +12,18 @@ class User(AbstractBaseUser):
 
     def __str__(self):
         return self.email
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=85)
+    enterprise = models.ForeignKey(Enterprise, on_delete=models.CASCADE)
+
+
+class GroupPermissions(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    permission = models.ForeignKey(Permission, on_delete=models.CASCADE)
+
+
+class UserGroups(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
